182a1fd16d6189a084f014731014620c704b8625
nabendu82 <nabendu.biswas@gmail.com>
2019-01-06 07:17:22 +0530

    Basic App Layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Hyderabadi Biryani', 'A really tasty hyderabadi biryani','https://www.maggime.com/sites/default/files/recipes/en/Chicken-Biryani.jpg'),
+    new Recipe('Beharoz Biryani', 'A tasty secret biryani for which a war was fought','https://b.zmtcdn.com/data/pictures/chains/6/18365226/526d9d73cdaf6700c056f48af5429de5_featured_v2.jpg?fit=around%7C400%3A400&crop=400%3A400%3B%2A%2C%2A')
+  ];
 
   constructor() { }
 
